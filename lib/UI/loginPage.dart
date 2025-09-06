3e662a2ad583a572027ae9173bd7ae664e2b59cc
dat1892003 <lethanhdat1892003@gmail.com>
2025-09-06 20:11:07 +0700

    Update UI login

---
+++ lib/UI/loginPage.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class loginPage extends StatefulWidget {
+  const loginPage({super.key});
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Icon(Icons.arrow_back),
+      ),
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(onPressed: (){}, child: Row(
+                      children: [
+                        Text("Tiếng việt",style: TextStyle(color: Colors.black)),
+                        Icon(Icons.arrow_drop_down_outlined, color: Colors.black,),
+                      ],
+                    ))
+                  ],
+                ),
+              ),
+              Flexible(flex: 1,child: Container( margin: EdgeInsets.only(top: 20), child: Icon(Icons.facebook, color: Colors.blueAccent,size: 100,)),),
+              Flexible(
+                  flex: 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 10, right: 10, top: 50),
+                            padding: EdgeInsets.only(left: 10),
+                            decoration: BoxDecoration(border: Border.all(width: 1), borderRadius: BorderRadius.circular(15)),
+                            child: TextField(decoration: InputDecoration(labelText: "Nhập vào email hoặc số điện thoại", border: InputBorder.none),),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 10, right: 10, top: 20),
+                            padding: EdgeInsets.only(left: 10),
+                            decoration: BoxDecoration(border: Border.all(width: 1), borderRadius: BorderRadius.circular(15)),
+                            child: TextField(decoration: InputDecoration(labelText: "Nhập vào mật khẩu", border: InputBorder.none),),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 10, right: 10, top: 20),
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  minimumSize: Size(double.infinity, 50),
+                                ),
+                                onPressed: (){},
+                                child: Text("Đăng nhập", style: TextStyle(color: Colors.white),)
+                            ),
+                          ),
+                          TextButton(onPressed: (){},style: TextButton.styleFrom(padding:EdgeInsets.only(top: 15)),
+                              child: Text("Quên mật khẩu?", style: TextStyle(color: Colors.black),))
+                        ],
+                      )
+
+                    ],
+                  )
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(left: 10, right: 10),
+                          decoration: BoxDecoration(border: Border.all(color: Colors.blue, width: 2),borderRadius: BorderRadius.circular(20) ),
+                          child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 50)),
+                              child: Text("Tạo tài khoản mới",style: TextStyle(color: Colors.blue),))
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(FontAwesomeIcons.facebookMessenger, color: Colors.pinkAccent,),
+                            SizedBox(width: 10,),
+                            Text("From Meta")
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:html';
-
+import 'package:clone_fb/UI/loginPage.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,63 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const HomePage(),
       debugShowCheckedModeBanner: false,
+      home: loginPage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-  late TabController tabController;
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 3, vsync: this);
-  }
-  Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-            title: Text("facebook"),
-            actions: [
-              Container(
-                  margin: EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.grey.withOpacity(0.4)),
-                  child: IconButton(onPressed: (){}, icon: Icon(Icons.search))
-              ),
-              Container(
-                  margin: EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.grey.withOpacity(0.4)),
-                  child: IconButton(onPressed: (){}, icon: Icon(Icons.menu))
-              ),
-            ],
-            bottom: TabBar(
-                controller: tabController,
-                tabs: [
-                  Tab(icon: Icon(Icons.home),),
-                  Tab(icon: Icon(Icons.group),),
-                  Tab(icon: Icon(FontAwesomeIcons.facebookMessenger),),
-                  Tab(icon: Icon(Icons.video_library)),
-                  Tab(icon: Icon(Icons.notifications)),
-                  Tab(icon: Icon(Icons.storefront)),
-                  ]) ,
-        ),
-        body: TabBarView(
-          controller: tabController,
-          children: [
-            Center(child: Text("Home Screen")),
-            Center(child: Text("Favorite Screen")),
-            Center(child: Text("Settings Screen")),
-          ],
-        )
-    );
-  }
-}
 
 
