5a19967d75fa69948b16e400527c0c3d98650e35
dat1892003 <lethanhdat1892003@gmail.com>
2025-09-10 21:55:59 +0700

    update UI

--- lib/UI/homePage.dart
+++ lib/UI/homePage.dart
@@ -356,6 +356,7 @@ class _HomePageState extends State<HomePage>
                         ),
                         Flexible(
                             flex: 5,
+                            fit: FlexFit.loose,
                             child: Image(
                               image: NetworkImage(
                                   "https://images2.thanhnien.vn/528068263637045248/2024/1/25/e093e9cfc9027d6a142358d24d2ee350-65a11ac2af785880-17061562929701875684912.jpg"),
