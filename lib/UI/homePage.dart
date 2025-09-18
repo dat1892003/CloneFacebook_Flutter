c0ca41761e67a9ed92bcf29569ce6c4e9e907fd6
dat1892003 <lethanhdat1892003@gmail.com>
2025-09-18 21:42:23 +0700

    update

--- lib/UI/homePage.dart
+++ lib/UI/homePage.dart
@@ -90,9 +90,7 @@ class _HomePageState extends State<HomePage>
                         )),
                   ],
                 );
-                if (result != null) {
-                  print("Bạn chọn: $result");
-                }
+
               },
             ),
             IconButton(onPressed: () {}, icon: Icon(Icons.search)),
