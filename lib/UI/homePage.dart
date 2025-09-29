f31d9a020a4ede062e64734ee9ace59e74a92f98
dat1892003 <lethanhdat1892003@gmail.com>
2025-09-29 22:16:09 +0700

    update

--- lib/UI/homePage.dart
+++ lib/UI/homePage.dart
@@ -206,7 +206,10 @@ class _HomePageState extends State<HomePage>
                     Container(width: 50, height: 50,margin: EdgeInsets.only(bottom: 10),padding: EdgeInsets.only(top: 4),decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(100),
                         color: Colors.grey[600]) ,child: Icon(Icons.favorite,size: 40, color: Colors.red,)),
-                    Icon(Icons.more_horiz)
+                    Container(width: 50, height: 50,margin: EdgeInsets.only(bottom: 10),padding: EdgeInsets.only(top: 4),decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.grey[600]
+                    ) ,child: Icon(Icons.favorite,size: 40, color: Colors.red,)),
                   ],
                 ),)),
               ],
