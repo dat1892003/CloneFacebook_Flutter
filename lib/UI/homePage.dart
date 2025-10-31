1dc0ade57854306390a4a341bddeb13a506e02cf
dat1892003 <lethanhdat1892003@gmail.com>
2025-10-31 21:30:58 +0700

    Update UI

--- lib/UI/homePage.dart
+++ lib/UI/homePage.dart
@@ -340,8 +340,14 @@ class _HomePageState extends State<HomePage>
           ),
           Container(
             width: double.infinity,
+            height: 70,
             margin: EdgeInsets.only(left: 10, right: 10, top: 5),
-            child: ElevatedButton(onPressed: (){}, child: Text("Đăng xuất")),
+            child: ElevatedButton(onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent
+              ),
+              child: Text("Đăng xuất",style: TextStyle(color: Colors.white),),
+            ),
           ),
 
         ],
